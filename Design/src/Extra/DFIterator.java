8eeec32a68a3eb33e5feead9f15c129199533619
fredrikstrandberg <fredrik.strandberg@live.se>
2021-11-30 12:19:41 +0100

    Uppg1 klar Uppg2 klar Extra typ klar

--- Design/src/Extra/DFIterator.java
+++ Design/src/Extra/DFIterator.java
@@ -1,15 +1,31 @@
 package Extra;
 
 import java.util.Iterator;
+import java.util.Stack;
 
-public class DFIterator implements Iterator {
+public class DFIterator implements Iterator<Component> {
+    Stack<Component> compStack = new Stack<>();
 
-    public boolean hasNext() {
-        return false;
+    public DFIterator(Component startComp) {
+        compStack.push(startComp);
     }
 
-    public Object next() {
+    public boolean hasNext() {
+        return !compStack.isEmpty();
+    }
+
+    public Component next() {
+        if (this.hasNext()) {
+            Component next = compStack.pop();
+            addChildren(next);
+            return next;
+        }
         return null;
     }
 
+    public void addChildren(Component curComp) {
+        if (curComp.getChildren() != null) {
+            compStack.addAll(curComp.getChildren());
+        }
+    }
 }
