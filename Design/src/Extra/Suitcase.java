098a3a9986cb5ccad4f2cd3255fdd333facb6f4d
fredrikstrandberg <fredrik.strandberg@live.se>
2021-11-30 12:57:12 +0100

    Uppg1 klar Uppg2 klar Extra typ klar

--- Design/src/Extra/Suitcase.java
+++ Design/src/Extra/Suitcase.java
@@ -38,12 +38,27 @@ public class Suitcase {
 
         plasticBag.add(fruit); plasticBag.add(chocolate); // add Leafs to plastic bag
 
-        Iterator<Component> BF = new BFIterator(suitcase);
+/*        Iterator<Component> BF = new BFIterator(suitcase);
         System.out.println("\nBredden först:");
         suitcase.printContent(BF);
 
         Iterator<Component> DF = new DFIterator(suitcase);
         System.out.println("\nDjupet först:");
+        suitcase.printContent(DF);*/
+
+        Iterator<Component> BF = suitcase.createBFIterator();
+        System.out.println("\nBredden först:");
+        suitcase.printContent(BF);
+
+        Iterator<Component> DF = suitcase.createDFIterator();
+        System.out.println("\nDjupet först:");
         suitcase.printContent(DF);
+
+
+        System.out.println("\nTest:");
+        for (Iterator<Component> i = suitcase.iterator(); i.hasNext();) {
+            Component item = i.next();
+            System.out.println(item);
+        }
     }
 }
